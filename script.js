d7a01a8e0999cbae6540829fdb0fcde7209aaf86
Bartek0074 <bartekdo123@gmail.com>
2022-10-25 22:58:21 +0200

    added placeMark, cellClick, swapTurns functions

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const cells = document.querySelectorAll('.cell');
 
-let player = 'x';
+let playerTurn = 'x';
 
 const cellMouseEnter = e => {
     if (e.target.innerHTML === '') {
-        player === 'x' ? 
+        playerTurn === 'x' ? 
         e.target.innerHTML = '<i class="hover fa-solid fa-x"></i>' : 
         e.target.innerHTML = '<i class="hover fa-solid fa-o"></i>'
     }
@@ -17,6 +17,26 @@ const cellMouseLeave = e => {
     }
 }
 
+const placeMark = (e) => {
+    if (e.target.parentElement.innerHTML === '<i class="hover fa-solid fa-x"></i>') {
+        e.target.parentElement.innerHTML = '<i class="mark fa-solid fa-x"></i>'
+    }
+    else if (e.target.parentElement.innerHTML === '<i class="hover fa-solid fa-o"></i>') {
+        e.target.parentElement.innerHTML = '<i class="mark fa-solid fa-o"></i>'
+    }
+}
+
+const cellClick = e => {
+    placeMark(e);
+    swapTurns();
+}
+
+const swapTurns = () => {
+    playerTurn === 'x' ? playerTurn = 'o' : playerTurn = 'x'
+}
+
 cells.forEach(cell => cell.addEventListener('mouseenter', e => cellMouseEnter(e)));
 
 cells.forEach(cell => cell.addEventListener('mouseleave', e => cellMouseLeave(e)));
+
+cells.forEach(cell => cell.addEventListener('click', e => cellClick(e)));
