13a5bf9ff1ab5b2049083745fc3284882f126f6e
Bartek0074 <bartekdo123@gmail.com>
2022-10-26 22:32:26 +0200

    added checkWin, endGame functions and cutted some js code

--- script.js
+++ script.js
@@ -1,42 +1,92 @@
 const cells = document.querySelectorAll('.cell');
+const winningMessageTxt = document.querySelector('[data-winning-message-text]')
+const winningMessage = document.querySelector('.winning-message');
 
-let playerTurn = 'x';
+let crossTurn = true;
+
+const circleClass = 'o';
+const crossClass = 'x';
+
+const circleHoverInnerHTML = '<i class="hover fa-solid fa-o"></i>';
+const crossHoverInnerHTML = '<i class="hover fa-solid fa-x"></i>';
+const circleMarkInnerHTML = '<i class="mark fa-solid fa-o"></i>';
+const crossMarkInnerHTML = '<i class="mark fa-solid fa-x"></i>';
+
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 
 const cellMouseEnter = e => {
-    if (e.target.innerHTML === '') {
-        playerTurn === 'x' ? 
-        e.target.innerHTML = '<i class="hover fa-solid fa-x"></i>' : 
-        e.target.innerHTML = '<i class="hover fa-solid fa-o"></i>'
+    const cell = e.target;
+
+    if (cell.innerHTML === '') {
+        crossTurn ? cell.innerHTML = crossHoverInnerHTML : cell.innerHTML = circleHoverInnerHTML
     }
 }
 
 const cellMouseLeave = e => {
-    if (e.target.innerHTML === '<i class="hover fa-solid fa-o"></i>' 
-    || e.target.innerHTML === '<i class="hover fa-solid fa-x"></i>') {
-        e.target.innerHTML = ''
+    const cell = e.target
+
+    if (cell.innerHTML === crossHoverInnerHTML || cell.innerHTML === circleHoverInnerHTML) {
+        cell.innerHTML = ''
     }
 }
 
-const placeMark = (e) => {
-    if (e.target.parentElement.innerHTML === '<i class="hover fa-solid fa-x"></i>') {
-        e.target.parentElement.innerHTML = '<i class="mark fa-solid fa-x"></i>'
+const placeMark = (cell) => {
+    if (cell.parentElement.innerHTML === crossHoverInnerHTML) {
+        cell.parentElement.classList.add(crossClass);
+        cell.parentElement.innerHTML = crossMarkInnerHTML;
     }
-    else if (e.target.parentElement.innerHTML === '<i class="hover fa-solid fa-o"></i>') {
-        e.target.parentElement.innerHTML = '<i class="mark fa-solid fa-o"></i>'
+    else if (cell.parentElement.innerHTML === circleHoverInnerHTML) {
+        cell.parentElement.classList.add(circleClass);
+        cell.parentElement.innerHTML = circleMarkInnerHTML;
     }
 }
 
-const cellClick = e => {
-    placeMark(e);
-    swapTurns();
+const checkWin = (currentClass) => {
+    return winningCombinations.some(combination => {
+        return combination.every(index => {
+            return cells[index].classList.contains(currentClass)
+        })
+    })
+}
+
+const endGame = (draw) => {
+    if (draw) {
+
+    }
+    else {
+        winningMessageTxt.innerText = `${crossTurn ? "X's wins!" : "O's wins!"}`
+    }
+    winningMessage.classList.add('show');
 }
 
 const swapTurns = () => {
-    playerTurn === 'x' ? playerTurn = 'o' : playerTurn = 'x'
+    crossTurn = !crossTurn;
+}
+
+const cellClick = e => {
+    const cell = e.target;
+    const currentClass = crossTurn ? crossClass : circleClass;
+
+    placeMark(cell);
+
+    if(checkWin(currentClass)) {
+        endGame(false)
+    }
+
+    swapTurns();
 }
 
 cells.forEach(cell => cell.addEventListener('mouseenter', e => cellMouseEnter(e)));
 
 cells.forEach(cell => cell.addEventListener('mouseleave', e => cellMouseLeave(e)));
 
-cells.forEach(cell => cell.addEventListener('click', e => cellClick(e)));
+cells.forEach(cell => cell.addEventListener('click', e => cellClick(e), { once: true }));
